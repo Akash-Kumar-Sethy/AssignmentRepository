64f587b8661c0b9f34889b83ed1e6101d9b2f5e0
Akash-Kumar-Sethy <aakashsethy@outlook.com>
2021-07-05 17:12:01 +0530

    Completed Assignment

---
+++ 46094232- Akash-Kumar-Sethy/Staff.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp
+{
+    class Staff
+    {
+        public int StaffID;
+        public Staff(string name,int id) {
+            StaffName = name;
+            StaffID=id;
+        }
+        public string StaffName{ set; get; }
+    }
+}
---
+++ 46094232- Akash-Kumar-Sethy/Student.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp
+{
+    class Student
+    {
+        private static int studentId = 0;
+        public Student() {
+            StudentID=++studentId;
+        }
+        public Student(string name, DateTime dob, string gender, int sclass, char section,string staffName)
+        {
+            StudentID = ++studentId;
+            StudentName = name;
+            DOB = dob;
+            Gender = gender;
+            StudentClass = sclass;
+            Section = section;
+            StaffName = staffName;
+        }
+        public int StudentID { get; set; }
+        public string StudentName { get; set; }
+        public DateTime DOB;
+        public string Gender{ set; get; }
+        public int StudentClass { set; get; }
+        public char Section { set; get; } 
+        public string StaffName;
+    }
+}
